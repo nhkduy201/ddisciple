9e42352e85c2e2cf2ea07549504af6c87fa1e80f
nhkduy201 <nhkduy201@gmail.com>
2022-06-09 23:35:37 +0700

    google translator speak

--- index.js
+++ index.js
@@ -1,158 +1,167 @@
-import fs from 'fs';
-import got from 'got';
-import client from 'soundoftext-js';
-import { Client, Intents } from 'discord.js';
-import { joinVoiceChannel, createAudioPlayer, createAudioResource, AudioPlayerStatus } from '@discordjs/voice';
-const bot = new Client({
+import Queue from './queue.js';
+import fs from 'fs'
+import got from 'got'
+import sotClient from 'soundoftext-js'
+import { Client, Intents } from 'discord.js'
+import { joinVoiceChannel, createAudioPlayer, createAudioResource, AudioPlayerStatus } from '@discordjs/voice'
+import dotenv from 'dotenv'
+dotenv.config()
+const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES,
      Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_VOICE_STATES],
   partials: ['MESSAGE', 'CHANNEL', 'REACTION']
-});
-const botToken = 'OTY4ODQ3MDIwMzQ3NTEwODY1.GP7oMM.ndVdeUbCp3R_n8v4bMC63NPeZp1CJmGdgCy17k';
+})
 
-let player = createAudioPlayer();
-let mp3Files;
-let resource;
-let connection;
-let playMessage;
-let preVolume = 1;
-let resourceStack = [];
+let player = createAudioPlayer()
+let mp3Files
+let resource
+let connection
+let playMessage
+let preVolume = 1
+let queue = new Queue()
 
-const play = (input) => {
-  resource = createAudioResource(input, { inlineVolume: true });
-  player.play(resource);
-}
+player.on(AudioPlayerStatus.Idle, (oldState, newState) => {
+  resource = undefined
+  play()
+})
 
-const loopResourceListener = (oldState, newState) => {
-  play();
-}
-
-const loopResource = () => {
-  player.on(AudioPlayerStatus.Idle, loopResourceListener);
-}
-
-const noLoopResource = () => {
-  player.removeListener(AudioPlayerStatus.Idle, loopResourceListener);
+const play = () => {
+  if(player.state.status === AudioPlayerStatus.Idle && !queue.isEmpty) {
+    resource = createAudioResource(queue.dequeue(), { inlineVolume: true })
+    player.play(resource)
+  }
 }
 
 const initConnection = () => {
   connection = joinVoiceChannel({
-    channelId: '974291777500639255',
-    guildId: '974291776993108059',
-    adapterCreator: bot.channels.cache.find(channel => channel.id === '974291777500639255').guild.voiceAdapterCreator,
-  });
-  connection.subscribe(player);
+    channelId: process.env.VOICE_CHANNEL_ID,
+    guildId: process.env.SERVER_ID,
+    adapterCreator: client.channels.cache.find(channel => channel.id === process.env.VOICE_CHANNEL_ID).guild.voiceAdapterCreator,
+  })
+  connection.subscribe(player)
 }
 
-const readMp3Files = () => {
-  fs.readdir('mp3/', (err, files) => {
-    if (err)
-      console.log(err.message);
-    else
-      mp3Files = files.filter((file) => file.endsWith('.mp3'));
-  });
-}
-
-const sendInValid = (message) => message.channel.send('Invalid command!');
-const sendOutOfRange = (message) => message.channel.send('Number must be in range!');
-const sendNoFile = (message) => message.channel.send('Sorry there\'s no files!');
-const sendPlayInfo = (message, whatPlaying) => message.channel.send('Playing: ' + whatPlaying);
+const sendInValid = (message) => message.channel.send('Invalid command!')
+const sendOutOfRange = (message) => message.channel.send('Number must be in range!')
+const sendNoFile = (message) => message.channel.send('There\'s no files!')
 
 const volumeControl = (reaction, user) => {
-  if (resource.started && !resource.ended && playMessage
-    && user.id != '968847020347510865' && reaction.message.id === playMessage.id) {
+  if (resource && resource.started && !resource.ended && playMessage
+    && user.id != client.user.id && reaction.message.id === playMessage.id) {
     if (reaction.emoji.name === '🔊') {
       if(resource.volume.volume + 0.25 <= 1) {
-        resource.volume.volume += 0.25;
-        preVolume = null;
+        resource.volume.volume += 0.25
+        preVolume = null
       }
     }
     if (reaction.emoji.name === '🔉') {
       if(resource.volume.volume - 0.25 >= 0) {
-        resource.volume.volume -= 0.25;
-        preVolume = null;
+        resource.volume.volume -= 0.25
+        preVolume = null
       }
     }
     if (reaction.emoji.name === '🔇') {
       if (resource.volume.volume == 0) {
-          resource.volume.volume = preVolume || 0.25;
+          resource.volume.volume = preVolume || 0.25
       } else {
-        preVolume = resource.volume.volume;
-        resource.volume.volume = 0;
+        preVolume = resource.volume.volume
+        resource.volume.volume = 0
       }
     }
   }
 }
 
-const bulkDelete = () => setInterval(() => bot.channels.cache.find(channel => channel.id == '982529141477875781').bulkDelete(100), 2000);
+const sendPlayInfo = (message, whatPlaying) => {
+  message.channel.send('Playing: ' + whatPlaying).then(afterMess => {
+    playMessage = afterMess
+    playMessage.react('🔊')
+    playMessage.react('🔉')
+    playMessage.react('🔇')
+  })
+}
 
-bot.on('ready', () => {
-  readMp3Files();
+const handlePlayMp3 = (message, playPara) => {
+  sendPlayInfo(playPara.substring(message, playPara.lastIndexOf('/') + 1))
+  queue.enqueue(got.stream(playPara))
+}
+
+const handleSpeak = async (message, playPara) => {
+  playPara = playPara.replace(new RegExp(process.env.SECRET_1, 'g'), decodeURI(process.env.SECRET_2))
+  .replace(new RegExp(process.env.SECRET_3, 'g'), decodeURI(process.env.SECRET_4))
+  sendPlayInfo(message, 'Speaking...')
+  let urlPromises = [];
+  let i = 0
+  let j
+  while(true) {
+    j = i + 100
+    if(j >= playPara.length) {
+      urlPromises.push(sotClient.sounds.create({ text: playPara.substring(i), voice: 'vi-VN' }))
+      break
+    }
+    j = playPara.lastIndexOf(' ', j)
+    if(j == -1) {
+      j = playPara.length
+      continue
+    }
+    urlPromises.push(sotClient.sounds.create({ text: playPara.substring(i, j), voice: 'vi-VN' }))
+    i = j + 1
+  }
+  Promise.all(urlPromises).then(urls => {
+    for(const url of urls) {
+      queue.enqueue(got.stream(url));
+    }
+    play();
+  })
+}
+
+const bulkDelete = (channelId) => setInterval(() => client.channels.cache.find(channel => channel.id == channelId).bulkDelete(100), 2000)
+
+client.on('ready', () => {
+  console.log('client is ready')
 })
   .on('messageReactionAdd', volumeControl).on('messageReactionRemove', volumeControl)
   .on('messageCreate', async message => {
-    if (message.content.startsWith('$play')) {
-      let playPara = message.content.replace(/\s\s+/g, ' ').split(' ').splice(1).join(' ');
+    if (message.content.startsWith('$play') && message.author.id === '487623378514214925') {
+      let playPara = message.content.replace(/\s\s+/g, ' ').split(' ').splice(1).join(' ').trim()
       if(!playPara.length) {
-        sendInValid(message);
-        return;
+        sendInValid(message)
+        return
       }
       // check if number, play mp3
-      let fileNumber = parseInt(playPara, 10);
-      let playObj = {};
+      let fileNumber = parseInt(playPara, 10)
+      let playObj = {}
       if (!connection || connection.state.status === 'destroyed') {
-        initConnection();
+        initConnection()
       }
-      if (!isNaN(fileNumber)) {
-        if (!mp3Files.length) {
-          sendNoFile(message);
-          return;
-        }
-        if (fileNumber < 1 || fileNumber > mp3Files.length) {
-          sendOutOfRange(message);
-          return;
-        }
-        playObj.info = mp3Files[fileNumber - 1];
-        playObj.input = fs.createReadStream('mp3/' + playObj.info);
-      } else if(playPara.startsWith('http') && playPara.endsWith('.mp3')){
-        playObj.info = playPara.substring(playPara.lastIndexOf('/') + 1);
-        playObj.input = got.stream(playPara);
+      if(playPara.startsWith('http') && playPara.endsWith('.mp3')) {
+        handlePlayMp3(message, playPara)
       } else {
-        playPara = playPara.replace(/^m\s+|\s+m\s+/g,' mày ');
-        let soundUrl = await client.sounds.create({ text: playPara, voice: 'vi-VN' });
-        playObj.input = got.stream(soundUrl);
-        let infoLen = Math.max(parseInt(playPara.length / 3), 10);
-        playObj.info = playPara.substring(0, infoLen) + '...';
+        await handleSpeak(message, playPara)
       }
-      sendPlayInfo(message, playObj.info).then(afterMess => {
-        playMessage = afterMess;
-        playMessage.react('🔊');
-        playMessage.react('🔉');
-        playMessage.react('🔇');
-      });
-      play(playObj.input);
+      play()
     }
-    if (message.content == '$stop') {
-      player.stop();
+    if (message.content == '$pause') {
+      player.pause()
+    }
+    if (message.content == '$resume') {
+      player.unpause()
     }
     if (message.content == '$list') {
-      message.channel.send(mp3Files
-        .map((file, idx) => `${(idx + 1).toString()}. ${file}`)
-        .join('\n'));
+      let listContent = mp3Files
+      .map((file, idx) => `${(idx + 1).toString()}. ${file}`)
+      .join('\n')
+      if(listContent.length)
+        message.channel.send(listContent)
+      else
+        message.channel.send('No file found!')
     }
     if (message.content == '$status') {
-      console.log("Connection status:", connection.state.status);
-      console.log("Player status:", player.state.status);
+      console.log("Connection status:", connection.state.status)
+      console.log("Player status:", player.state.status)
     }
-    if (message.content == '$loop') {
-      loopResource();
-    }
-    if (message.content == '$noloop') {
-      noLoopResource();
-    }
-    if (message.content == '$bye') {
-      player.stop();
-      if (connection) connection.destroy();
+    if (message.content == '$stop') {
+      player.stop()
+      if (connection) connection.destroy()
     }
   })
-  .login(botToken);
+  .login(process.env.BOT_TOKEN)
