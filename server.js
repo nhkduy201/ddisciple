2d398dd7849c0d74c8736fab5744778460f8cbc7
nhkduy201 <nhkduy201@gmail.com>
2022-06-10 15:54:26 +0700

    fix server, add favicon

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
-const express = require('express');
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import express from 'express';
+import path from 'path';
 const server = express();
-const path = require('path');
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 server.all('/', (req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, './index.html'));
 });
 
-module.exports = () => server.listen(process.env.PORT || 5000, '0.0.0.0');
+export default () => server.listen(process.env.PORT || 5000, '0.0.0.0');
