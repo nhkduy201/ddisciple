c313bb21661392dffb2159db41631a80608798b6
nhkduy201 <nhkduy201@gmail.com>
2022-06-14 12:23:51 +0700

    send 200

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const server = express();
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 server.all('/', (req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, './index.html'));
+  res.status(200).sendFile(path.join(__dirname, './index.html'));
 });
 
 export default () => server.listen(process.env.PORT || 5000, '0.0.0.0');
